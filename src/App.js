9e55195708104f345dbc597f74b3f58979d6f354
Quvonchbek0203 <150164830+Quvonchbek0203@users.noreply.github.com>
2024-02-15 22:25:40 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+// import { FiMoreVertical } from "react-icons/fi";
+import Tailwind from './components/tailwind';
+
+const App = () => {
+  return (
+    <div>
+      <Tailwind/>
+    </div>
+    // <div className='flex justify-center items-center'>
+    //   <div className='w-[800px] h-[300px]'>
+    //     <table>
+
+    //       <tr className='grid grid-cols-4 justify-center'>
+    //         <th className='w-[400px]'>CLIENT/INVOICE</th>
+    //         <th className='w-[200px]'>AMOUNT</th>
+    //         <th className='w-[200px]'>ISSUED/DUE</th>
+    //         <th className='w-[200px]'>STATUS</th>
+    //       </tr>
+          
+    //       <tr className='flex justify-around items-center gap-16 pr-5'>
+    //         <td className='flex justify-center items-center gap-5'>
+    //           <div>
+    //             <img src="https://images.squarespace-cdn.com/content/v1/643692de1691a04141c1756d/6bdfd3ed-c0db-42b4-9925-94688b742537/7J5A3495.jpg?format=1500w" alt="molly" className='w-20 h-20 rounded-full'/>
+    //           </div>
+    //           <div>
+    //             <p>Molly Sanders</p>
+    //             <p>000004</p>  
+    //           </div>
+    //         </td>
+    //         <td>
+    //           <p>$20,000</p>
+    //           <p>USD</p>
+    //         </td>
+    //         <td>
+    //           <p>Sept 28,2019</p>
+    //           <p>Due in 3 days</p>
+    //         </td>
+    //         <td>
+    //           <button className=''>Paid</button>
+    //         </td>
+    //         <td><FiMoreVertical /></td>
+    //       </tr>
+
+    //       </table>
+    //   </div>
+    // </div>
+  )
+}
+
+export default App
---
+++ src/components/tailwind.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Tailwind = () => {
+  return (
+    <div className='w-full h-[100vh] flex justify-center items-center bg-slate-800'>
+      <div className='w-[400px] rounded-xl border-white border-solid border-2 p-5 flex flex-col gap-6 bg-slate-600'>
+        <div >
+            <h1 className='text-2xl font-bold'>Sign in to your account</h1>
+        </div>
+        <div>
+            <p className='font-bold pb-2'>Your email</p>
+            <input type="email" placeholder='name@company.com' className='bg-slate-300  p-2 w-full rounded-lg'/>
+        </div>
+        <div>
+            <p className='font-bold pb-2'>Password</p>
+            <input type="password" className='bg-slate-300 p-2 w-full rounded-lg' placeholder='******' />
+        </div>
+        <div className='flex justify-between'>
+            <div className='flex gap-2'>
+                <input type="checkbox" />
+                <p>Remember me</p>
+            </div>
+            <a href="# " className='text-blue-600'>Forgot password?</a>
+        </div>
+        <button className='w-full h-10 rounded-xl bg-blue-800 rounded-lg border-solid '>Sign in</button>
+        <div>
+            <p>Don’t have an account yet?   <a href="# " className='text-blue-600'>Sign up</a></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Tailwind
